2ccd8f5cc24fe1620cb5f42547798aa02c331339
shecodez - Nicole J. Nobles <shecodez@gmail.com>
2017-11-05 00:41:58 -0400

    Add http and socket.io packages to api

--- holospace-api/src/server.js
+++ holospace-api/src/server.js
@@ -11,16 +11,33 @@ dotenv.config();
 
 const app = express();
 
+/*
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', (socket) {
+  console.log('connection established.');
+  socket.on("new-message", (message) =>{
+    console.log(message);
+    io.emit("receive-message", message);
+  });
+});
+
+http.listen('2000', () => {
+  console.log('connected');
+});
+*/
+
 app.use(bodyParser.json());
 
 app.use('/api/auth', auth);
-
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
-})
+});
 
-//app.listen(8080, () => console.log('Running on localhost:8080'));
-const server = app.listen(8080, function() {
+const port = process.env.PORT || 8080;
+//app.listen(port, () => console.log('Running on localhost:'+port));
+const server = app.listen(port, function() {
   mongoose.Promise = bluebird;
   mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
 
@@ -29,8 +46,7 @@ const server = app.listen(8080, function() {
     console.error(err);
     process.exit(1);
   });
-
   db.once('open', () => {
-    console.log('Express server listening on %d, in %s mode', 8080, app.get('env'));
+    console.log('Express server listening on %d, in %s mode', port, app.get('env'));
   });
 });
